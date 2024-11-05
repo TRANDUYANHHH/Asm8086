38256421f965f3d62a1c9bcb77aba854d65d7097
TRANDUYANHHH <162913482+TRANDUYANHHH@users.noreply.github.com>
2024-11-06 00:22:20 +0700

    Update b3.asm

--- b3.asm
+++ b3.asm
@@ -1,26 +1,56 @@
-.Model Small
-
-.Stack 100H
-
-.Data
-    char db 'A'; ki tu trong data segment
-    nl db 13, 10, '$';xuong dong
-.Code
-Main Proc
-    ;Khoi tao doan du lieu
+.model small
+.stack 100h
+.data 
+    arr db -11, 2, 3, -4, 5, 6, -7, 8, 9, -10
+    sum db 0
+.code
+main proc
     mov ax, @Data
-    mov ds, ax   
+    mov ds, ax
     
-    cmp char, 'Z'; so sanh char voi ki tu Z
-    jle OUTPUT; Neu nho hon hoac bang, tuc la ki tu in hoa ta nhay den nhan OUTPUT de in ra man hinh
-    sub char, 32; Khong thi ta se tru ki tu di 32 (chuyen ve in hoa)
+    mov ax, 0
+    lea si, arr
+    mov cx, 10
+tong:
+    lodsb
+    cmp al, 0
+    jge pass
+    mov bl, 0
+    sub bl, al 
+    add sum, bl
+pass:
+    loop tong
     
-OUTPUT:
-    mov dl, char
+    mov dl, '-'
     mov ah, 2
     int 21h
     
+    mov ah, 0
+    mov al, sum
+    call print
+    
     mov ah, 4ch
-    int 21h 
-Main Endp
-end
+    int 21h
+main endp  
+
+print proc
+    mov bx, 10
+    mov cx, 0
+chia:
+    mov dx, 0
+    div bx
+    push dx
+    inc cx
+    cmp ax, 0
+    je hienthi
+    jmp chia
+hienthi:
+    pop dx
+    add dl, '0'
+    mov ah, 2
+    int 21h
+    dec cx
+    cmp cx, 0
+    jne hienthi
+    ret
+print endp
