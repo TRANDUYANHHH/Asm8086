4b16aa96d62f3f93ced43a97f38e348414564722
TRANDUYANHHH <162913482+TRANDUYANHHH@users.noreply.github.com>
2024-11-07 00:17:30 +0700

    Update COAF24PRJ053P12.asm

--- COAF24PRJ053P12.asm
+++ COAF24PRJ053P12.asm
@@ -18,20 +18,20 @@
 main proc
     mov ax, @Data
     mov ds, ax
-    ; In ra xau ki tu yeu cau
+    
     mov ah, 9
     lea dx, msg
     int 21h
-    ; Nhap du lieu vao thanh ghi AL
+    
     mov ah, 1
     int 21h
-    mov datain, al ;Chuyen du lieu thanh ghi al vao bien datain 
-    ; In ra dau xuong dong 
+    mov datain, al 
+   
     mov ah, 9
     lea dx, endline
     int 21h
-    cmp datain, '0'; So sanh du lieu voi ki tu '0'
-    je VIETNAM ; Nhay neu bang
+    cmp datain, '0'
+    je VIETNAM 
     
     mov ah, 9
     lea dx, chao2
