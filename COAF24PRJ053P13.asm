39500b77bb29e9f23aa96897cf64e5b5114cc99b
TRANDUYANHHH <162913482+TRANDUYANHHH@users.noreply.github.com>
2024-11-07 00:03:33 +0700

    Update COAF24PRJ053P13.asm

--- COAF24PRJ053P13.asm
+++ COAF24PRJ053P13.asm
@@ -1,3 +1,9 @@
+;---------------------------------- Nhap vao 1 ki tu va in ra ki tu in hoa -----------------------------------
+; Ngo Quy Truong Giang - B23DCAT080
+; Tran Duy Anh - B23DCAT020
+; Vu Do Phuong Dong - B23DCAT045
+; Le Hong Son - B23DCAT260
+
 .model small
 .stack 100h
 .data 
@@ -7,9 +13,9 @@ main proc
     mov ax, @Data
     mov ds, ax   
    
-    cmp char, 'Z'
-    jle print
-    sub char, 32
+    cmp char, 'Z'; so sanh ki tu voi ki tu 'Z'
+    jle print    ; in ra luon neu la ki tu in hoa
+    sub char, 32 ; neu khong thi phai tru di 32
 print:
     mov dl, char
     mov ah, 2
