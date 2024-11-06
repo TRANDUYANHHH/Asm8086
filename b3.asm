664fce55d92c07808aaef237ecd5c49ba03ec684
TRANDUYANHHH <162913482+TRANDUYANHHH@users.noreply.github.com>
2024-11-06 09:58:22 +0700

    Update b3.asm

--- b3.asm
+++ b3.asm
@@ -1,56 +1,18 @@
 .model small
 .stack 100h
 .data 
-    arr db -11, 2, 3, -4, 5, 6, -7, 8, 9, -10
-    sum db 0
+    char db 'C'
 .code
 main proc
     mov ax, @Data
-    mov ds, ax
-    
-    mov ax, 0
-    lea si, arr
-    mov cx, 10
-tong:
-    lodsb
-    cmp al, 0
-    jge pass
-    mov bl, 0
-    sub bl, al 
-    add sum, bl
-pass:
-    loop tong
-    
-    mov dl, '-'
+    mov ds, ax   
+   
+    cmp char, 'Z'
+    jle print
+    sub char, 32
+print:
+    mov dl, char
     mov ah, 2
     int 21h
-    
-    mov ah, 0
-    mov al, sum
-    call print
-    
-    mov ah, 4ch
-    int 21h
-main endp  
-
-print proc
-    mov bx, 10
-    mov cx, 0
-chia:
-    mov dx, 0
-    div bx
-    push dx
-    inc cx
-    cmp ax, 0
-    je hienthi
-    jmp chia
-hienthi:
-    pop dx
-    add dl, '0'
-    mov ah, 2
-    int 21h
-    dec cx
-    cmp cx, 0
-    jne hienthi
-    ret
-print endp
+main endp
+end 
